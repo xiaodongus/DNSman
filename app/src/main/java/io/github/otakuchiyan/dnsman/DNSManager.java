2f426822b250998d82a96e3f79ca3edc08510815
otakuchiyan <codeotakuchiyan@gmail.com>
2015-05-03 14:11:44 +0800

    update

--- app/src/main/java/io/github/otakuchiyan/dnsman/DNSManager.java
+++ app/src/main/java/io/github/otakuchiyan/dnsman/DNSManager.java
@@ -1,19 +1,56 @@
 package io.github.otakuchiyan.dnsman;
 
+import android.content.Intent;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.util.Log;
+import android.content.Context;
 import java.io.IOException;
 import java.io.DataOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.net.InetAddress;
+import java.util.UnknownFormatConversionException;
+import java.net.UnknownHostException;
 
 
 /**
  * Created by nanafa on 2015/5/1.
  */
 public class DNSManager {
+    /*
+    public int setWiFiDNS(Context context, String DNS1, String DNS2) throws NoSuchFieldException, IllegalAccessException, UnknownHostException {
+        WifiConfiguration wc = null;
+        WifiManager wm = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wi = wm.getConnectionInfo();
+        List<WifiConfiguration> configuredNetworks = wm.getConfiguredNetworks();
+        for(WifiConfiguration w : configuredNetworks){
+            if(w.networkId == wi.getNetworkId()){
+                wc = w;
+                break;
+            }
+        }
+
+        Object lpf = wc.;
+        if(lpf == null){
+            return 1;
+        }
+        Object lpdf = wc.getDeclaredField("mDnses").get(wc);
+        ArrayList<InetAddress> DNSes = (ArrayList<InetAddress>)lpdf;
+        DNSes.clear();
+        DNSes.add(InetAddress.getByName(DNS1));
+        DNSes.add(InetAddress.getByName(DNS2));
+        return 0;
+    }
+    */
+
     public int setMobileNetDNS(String DNS1, String DNS2){
 
         if(DNS1 != null && DNS2 != null) {
             Log.d("DNSManager", DNS1);
             Log.d("DNSManager", DNS2);
+
             DataOutputStream dos = null;
 
             try {
--- app/src/main/java/io/github/otakuchiyan/dnsman/NetworkCheckReceiver.java
+++ app/src/main/java/io/github/otakuchiyan/dnsman/NetworkCheckReceiver.java
@@ -12,12 +12,15 @@ import android.text.TextUtils;
 import android.widget.Toast;
 import android.util.Log;
 
+import java.util.UnknownFormatFlagsException;
+
 import io.github.otakuchiyan.dnsman.DNSManager;
+import java.net.UnknownHostException;
 
 public class NetworkCheckReceiver extends BroadcastReceiver {
     final String CONNECTIVITY_CHANGE_ACTION = new String("android.net.conn.CONNECTIVITY_CHANGE");
 
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(Context context, Intent intent){
         DNSManager dns = new DNSManager();
         ConnectivityManager connmgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         if (connmgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED) {
@@ -32,13 +35,35 @@ public class NetworkCheckReceiver extends BroadcastReceiver {
             //FIXME: return DNS value use setting's value.
             String mDNS1 = sp.getString("mDNS1", "8.8.8.8");
             String mDNS2 = sp.getString("mDNS2", "8.8.4.4");
+
             Log.d("NCR", mDNS1);
             Log.d("NCR", mDNS2);
+
             if (dns.setMobileNetDNS(mDNS1, mDNS2) == 1) {
                 Toast.makeText(context, R.string.mobilenet_change_failed, Toast.LENGTH_LONG).show();
             } else {
                 Toast.makeText(context, R.string.mobilenet_changed, Toast.LENGTH_LONG).show();
             }
+        }/* else if (connmgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            SharedPreferences sp = context.getSharedPreferences("dnsconf", Context.MODE_PRIVATE);
+            String wDNS1 = sp.getString("wDNS1", "8.8.8.8");
+            String wDNS2 = sp.getString("wDNS2", "8.8.4.4");
+            try {
+                if (dns.setWiFiDNS(context, wDNS1, wDNS2) == 1) {
+                    Toast.makeText(context, R.string.wifi_change_failed, Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(context, R.string.wifi_changed, Toast.LENGTH_LONG).show();
+                }
+            } catch (NoSuchFieldException err){
+                err.printStackTrace();
+
+            } catch (IllegalAccessException err){
+                err.printStackTrace();
+            } catch (UnknownHostException err){
+                err.printStackTrace();
+            }
+        }
+        */
         }
     }
-}
+
